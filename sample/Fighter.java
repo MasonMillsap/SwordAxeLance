639cae4ec5dc4b4467638914b37be51d41e53d42
MasonMillsap <millsapms@hendrix.edu>
2017-02-28 14:15:58 -0600

    Update Fighter.java
    added stats and tostring

--- sample/Fighter.java
+++ sample/Fighter.java
@@ -1,3 +1,4 @@
+
 package sample;
 
 /**
@@ -7,11 +8,34 @@ public class Fighter {
     private Integer hitpoints;
     private String name;
     private Weapon weapon;
+    private HashMap<String, Integer> stats;
 
     public Fighter() {
         this.hitpoints = 10;
         this.weapon = null;
+        initializeStats();
+
     }
+    
+    private void initializeStats() {
+        stats = new HashMap();
+        stats.put("Attack", 0);
+        stats.put("Defense", 0);
+        stats.put("Vitality", 0);
+        stats.put("Speed", 0);
+    }
+
+    public String toString() {
+        String out = new String();
+        out += "Name:" + name + ":";
+        out += "HP:" + hitpoints.toString() + ":";
+        out += "Attack:" + stats.get("Attack") + ":";
+        out += "Defense:" + stats.get("Defense") + ":";
+        out += "Vitality:" + stats.get("Vitality") + ":";
+        out += "Speed:" + stats.get("Speed") + ":";
+        return out;
+    }
+ 
 
     public String getName() {return name;}
     public Integer getHP() {return hitpoints;}
@@ -20,6 +44,10 @@ public class Fighter {
     public void setName(String name) {this.name = name;}
     public void setHP(Integer hp) {this.hitpoints = hp;}
     public void setWeapon(Weapon weapon) {this.weapon = weapon;}
+    public void setStats(String stat, Integer value) {
+    stats.replace(stat, value);
+    }
+
 
     public void takeHit() {this.hitpoints -= 1;}
 
